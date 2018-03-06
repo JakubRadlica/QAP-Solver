f83cbd0dd734890edc7c62024d480af6d90e460d
Jakub Radlica <jakubradlica@gmail.com>
2018-03-06 19:15:18 +0100

    little parameters change

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -18,20 +18,20 @@ public class Application {
 
     public static void main(String[] args) throws ContextFileException {
 
-        Context context = new ContextReader().loadContextFromFile(ContextFile.QNP_20);
+        Context context = new ContextReader().loadContextFromFile(ContextFile.QNP_12);
 
         GeneticAlgorithm geneticAlgorithm =
                 new GeneticAlgorithm.GeneticAlgolrithmBuilder()
                         .context(context)
 //                        .selector(new RouletteSelector())
-                        .selector(new TournamentSelector(10))
+                        .selector(new TournamentSelector(5))
 //                        .selector(new RandomSelector())
                         .mutator(new RandomSwapMutator())
                         .crossover(new SimpleSwapCrossover())
                         .populationsNumber(100)
                         .generationsNumber(100)
                         .crossoverProbability(0.7)
-                        .mutateProbability(0.01)
+                        .mutateProbability(0.05)
                         .mutateSingle()
                         .build();
 
