a5f9b484542a20275bc444f30111f1cce2feb736
Jakub Radlica <jakubradlica@gmail.com>
2018-03-11 00:29:34 +0100

    Fix RouletteSelector

--- src/main/java/pl/radlica/operations/selector/RouletteSelector.java
+++ src/main/java/pl/radlica/operations/selector/RouletteSelector.java
@@ -6,6 +6,8 @@ import com.google.common.collect.TreeRangeMap;
 import pl.radlica.model.Genotype;
 import pl.radlica.model.Population;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 
@@ -14,15 +16,24 @@ public class RouletteSelector implements ISelector {
     @Override
     public Genotype select(Population population) {
 
+        List<Genotype> genotypes = population.getGenotypes();
         RangeMap<Integer, Genotype> roulette = TreeRangeMap.create();
-        int sum=0;
-        for(Genotype g: population.getGenotypes()){
-            roulette.put(Range.closed(sum, sum+g.getFittnes()-1), g);
-            sum+=g.getFittnes();
+
+        int sum = 0;
+        for (Genotype g : genotypes) {
+            sum += g.getFittnes();
+        }
+
+        int prSum = 0;
+        for (Genotype g : genotypes) {
+            int pr = sum - g.getFittnes();
+            roulette.put(Range.closed(prSum, prSum+pr), g);
+            prSum += pr;
         }
         Random random = new Random();
 
-        return roulette.get(random.nextInt(sum));
+
+        return roulette.get(random.nextInt(prSum));
     }
 
     @Override
